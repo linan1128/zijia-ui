082699829f91af6b85382baf7ac4f727a058c0c6
linan1128 <linan1128@hotmail.com>
2025-01-22 21:43:49 +0800

    new file:   docs/.vitepress/theme/base.css

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [UnoCSS()],
+  resolve: {
+    alias: [
+      {
+        find: /^.*\/base\.css$/,
+        replacement: fileURLToPath(
+          new URL('./.vitepress/theme/base.css', import.meta.url),
+        ),
+      },
+    ],
+  },
 })
